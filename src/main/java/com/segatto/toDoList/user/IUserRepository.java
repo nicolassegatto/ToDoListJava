15d3ac37e0ef23caa95517f0f7aaa865869768be
Nicolas Segatto <nicolasssegatto@icloud.com>
2023-10-13 12:28:57 -0300

    dockerFile Deploy

---
+++ src/main/java/com/segatto/toDoList/user/IUserRepository.java
@@ -0,0 +1,10 @@
+package com.segatto.toDoList.user;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID>{
+  UserModel findByUsername(String username);
+}
